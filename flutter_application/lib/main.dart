dc8fb391fedb2fef38859e2413cc48c1b1168c5b
ervinanggraini <ervinanggr@gmail.com>
2025-11-27 14:09:20 +0700

    chore: add .env support for Supabase credentials and update README with credits

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/core/extensions/hive_extensions.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -9,6 +10,7 @@ import 'package:flutter_application/dependency_injection.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
   await _initializeSupabase();
   await _initializeHive();
   configureDependencyInjection();
@@ -20,8 +22,8 @@ void main() async {
 
 Future<void> _initializeSupabase() async {
   await Supabase.initialize(
-    url: "https://fyuowtqduqjsgluqdcwj.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5dW93dHFkdXFqc2dsdXFkY3dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNjc4NTgsImV4cCI6MjA3OTc0Mzg1OH0.DZJRDuX1Riwhk_OXNo2PZPob2Hmsc_33ZXhpTlOff3Q",
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 }
 
